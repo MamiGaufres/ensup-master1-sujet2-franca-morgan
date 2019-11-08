c293f621e103157b9ffcf2c8d0530179b716c00d
MamiGaufres <mogofranca@live.fr>
2019-11-08 16:06:02 +0100

    V0.06B

--- src/service/Service.java
+++ src/service/Service.java
@@ -6,11 +6,13 @@ import metier.Personne;
 
 public class Service {
 	
+	//Créer une personne
 	public int creerPersonne(List<Personne> list, Personne personne) {
 		list.add(personne);
 		return 1;
 	}
 	
+	//Obtenir une personne par ID
 	public Personne getPersonne(List<Personne> list, int idPersonne) {
 		for(Personne x : list) {
 			if(x.getPersonneId() == idPersonne) {
@@ -20,8 +22,9 @@ public class Service {
 		return null;
 	}
 	
+
+	//Obtenir toutes les personnes
 	public static List<Personne> base = new ArrayList<Personne>();
-	
 	public List<Personne> findAllPersonnes() {
 		return (List<Personne>) base;
 	}
