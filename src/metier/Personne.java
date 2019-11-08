99049e160ba7fbd7f38563c3211a221ac73cc67b
MamiGaufres <mogofranca@live.fr>
2019-11-08 15:22:58 +0100

    V0.02

--- src/metier/Personne.java
+++ src/metier/Personne.java
@@ -1,5 +1,10 @@
 package metier;
 
 public class Personne {
-
+	private int personneId;
+	private String nom;
+	private String prenom;
+	private String login;
+	private String motDePasse;
+	private String dateNaissance;
 }
